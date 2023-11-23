b2c56814997344ad5e415d87e92534a2e34e486c
jigglypop <ydh2244@gmail.com>
2023-11-24 01:50:26 +0900

    1124

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gaesup-world.git/",
   plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
