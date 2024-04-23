9d87cfd9327de2c7aee171d71268be9dc8247880
jigglypop <ydh2244@gmail.com>
2024-04-24 00:50:41 +0900

    0424

--- src/gaesup/tools/minimap/index.tsx
+++ src/gaesup/tools/minimap/index.tsx
@@ -120,7 +120,7 @@ export function MiniMap(props: minimapType) {
             scale;
           const Z =
             (center.z - activeState.position.z) *
-            (props.angle ? -Math.cos(props.angle) : 1) *
+            (props.angle ? Math.cos(props.angle) : 1) *
             scale;
           return (
             <div key={key}>
