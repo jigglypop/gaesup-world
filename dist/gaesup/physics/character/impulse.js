944958490be8fe4ddf94e82b3fef4b31f9dd75f3
jigglypop <ydh2244@gmail.com>
2024-05-03 20:47:53 +0900

    0503

--- dist/gaesup/physics/character/impulse.js
+++ dist/gaesup/physics/character/impulse.js
@@ -1,17 +1,6 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 import { vec3 } from "@react-three/rapier";
 export default function impulse(prop) {
-    var rigidBodyRef = prop.rigidBodyRef, _a = prop.worldContext, states = _a.states, activeState = _a.activeState, clicker = _a.clicker, clickerOption = _a.clickerOption, dispatch = prop.dispatch;
+    var rigidBodyRef = prop.rigidBodyRef, _a = prop.worldContext, states = _a.states, activeState = _a.activeState;
     var isMoving = states.isMoving, isRunning = states.isRunning;
     var _b = prop.controllerContext.character, walkSpeed = _b.walkSpeed, runSpeed = _b.runSpeed, jumpSpeed = _b.jumpSpeed;
     var isOnTheGround = states.isOnTheGround, isJumping = states.isJumping;
@@ -34,10 +23,4 @@ export default function impulse(prop) {
     rigidBodyRef.current.applyImpulse(impulse, true);
     activeState.position = vec3(rigidBodyRef.current.translation());
     activeState.velocity = vec3(rigidBodyRef.current.linvel());
-    dispatch({
-        type: "update",
-        payload: {
-            activeState: __assign({}, activeState),
-        },
-    });
 }
--- dist/gaesup/physics/character/stop.js
+++ dist/gaesup/physics/character/stop.js
@@ -2,15 +2,21 @@ import { vec3 } from "@react-three/rapier";
 import { calcNorm } from "../../utils";
 export default function stop(prop) {
     var _a;
-    var rigidBodyRef = prop.rigidBodyRef, _b = prop.worldContext, control = _b.control, clicker = _b.clicker, mode = _b.mode, clickerOption = _b.clickerOption, activeState = _b.activeState;
+    var rigidBodyRef = prop.rigidBodyRef, _b = prop.worldContext, control = _b.control, clicker = _b.clicker, mode = _b.mode, clickerOption = _b.clickerOption;
     var keyS = control.keyS;
     if (keyS && mode.controller === "clicker") {
         clicker.isOn = false;
         clicker.isRun = false;
     }
-    // 목적지 도착 (클리커 막기 로직)
     var u = vec3((_a = rigidBodyRef.current) === null || _a === void 0 ? void 0 : _a.translation());
     var norm = calcNorm(u, clicker.point, false);
+    if (clickerOption.autoStart) {
+        clicker.isOn = true;
+        norm = calcNorm(u, clickerOption.queue[0], false);
+        var v = vec3(clickerOption.queue[0]);
+        var newAngle = Math.atan2(v.z - u.z, v.x - u.x);
+        clicker.angle = newAngle;
+    }
     if (norm < 1 && mode.controller === "clicker") {
         if (clickerOption.track && clickerOption.queue.length !== 0) {
             clicker.point = clickerOption.queue.shift();
