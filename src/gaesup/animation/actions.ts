a4a1d29416e846b3d5bbf894c7a3ce17b63b7691
jigglypop <52653682+jigglypop@users.noreply.github.com>
2024-09-24 14:09:40 -1200

    0924

--- src/gaesup/animation/actions.ts
+++ src/gaesup/animation/actions.ts
@@ -1,121 +1,112 @@
-import { useFrame } from "@react-three/fiber";
-import { useContext, useEffect } from "react";
-import { animationTagType } from "../controller/type";
-import { useGaesupAnimation } from "../hooks/useGaesupAnimation";
-import {
-  GaesupWorldContext,
-  GaesupWorldDispatchContext,
-} from "../world/context";
-import { playActionsType, subscribeActionsType } from "./type";
+import { useFrame } from '@react-three/fiber'
+import { useContext, useEffect } from 'react'
+import { animationTagType } from '../controller/type'
+import { useGaesupAnimation } from '../hooks/useGaesupAnimation'
+import { GaesupWorldContext, GaesupWorldDispatchContext } from '../world/context'
+import { playActionsType, subscribeActionsType } from './type'
 
 export function subscribeActions({ type }: subscribeActionsType) {
-  const { states } = useContext(GaesupWorldContext);
-  const { subscribeAll } = useGaesupAnimation({ type });
+  const { states } = useContext(GaesupWorldContext)
+  const { subscribeAll } = useGaesupAnimation({ type })
   // 초기 기본 애니메이션 등록
   useEffect(() => {
     subscribeAll([
       {
-        tag: "walk",
+        tag: 'walk',
         condition: () => !states.isRunning && states.isMoving,
         action: () => {},
-        animationName: "walk",
-        key: "walk",
+        animationName: 'walk',
+        key: 'walk',
       },
       {
-        tag: "run",
+        tag: 'run',
         condition: () => states.isRunning,
         action: () => {},
-        animationName: "run",
-        key: "run",
+        animationName: 'run',
+        key: 'run',
       },
       {
-        tag: "jump",
+        tag: 'jump',
         condition: () => states.isJumping,
         action: () => {},
-        animationName: "jump",
-        key: "jump",
+        animationName: 'jump',
+        key: 'jump',
       },
 
       {
-        tag: "ride",
-        condition: () => states.isPush["keyR"],
+        tag: 'ride',
+        condition: () => states.isPush['keyR'],
         action: () => {},
-        animationName: "ride",
-        key: "ride",
+        animationName: 'ride',
+        key: 'ride',
       },
       {
-        tag: "land",
+        tag: 'land',
         condition: () => states.isLanding,
         action: () => {},
-        animationName: "land",
-        key: "land",
+        animationName: 'land',
+        key: 'land',
       },
       {
-        tag: "fall",
+        tag: 'fall',
         condition: () => states.isFalling,
         action: () => {},
-        animationName: "fall",
-        key: "fall",
+        animationName: 'fall',
+        key: 'fall',
       },
-    ]);
-  }, []);
+    ])
+  }, [])
 }
 
-export default function playActions({
-  type,
-  actions,
-  animationRef,
-  currentAnimation,
-  isActive,
-}: playActionsType) {
-  const { mode, animationState, block } = useContext(GaesupWorldContext);
-  const dispatch = useContext(GaesupWorldDispatchContext);
-  const { notify, store } = useGaesupAnimation({ type });
+export default function playActions({ type, actions, animationRef, currentAnimation, isActive }: playActionsType) {
+  const { mode, animationState, block } = useContext(GaesupWorldContext)
+  const dispatch = useContext(GaesupWorldDispatchContext)
+  const { notify, store } = useGaesupAnimation({ type })
 
   if (isActive) {
-    currentAnimation = animationState?.[type]?.current;
+    currentAnimation = animationState?.[type]?.current
   }
 
   const play = (tag: keyof animationTagType) => {
-    animationState[type].current = tag;
-    const currentAnimation = store[tag];
+    animationState[type].current = tag
+    const currentAnimation = store[tag]
     if (currentAnimation?.action) {
-      currentAnimation.action();
+      currentAnimation.action()
     }
     dispatch({
-      type: "update",
+      type: 'update',
       payload: {
         animationState: {
           ...animationState,
         },
       },
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    let animation = "idle";
+    let animation = 'idle'
     if (block.animation) {
-      animation = "idle";
+      animation = 'idle'
     } else if (currentAnimation) {
-      animation = currentAnimation;
+      animation = currentAnimation
     } else if (animationState[type].current) {
-      animation = animationState[type].current;
+      animation = animationState[type].current
     }
-    const action = actions[animation]?.reset().fadeIn(0.2).play();
+    const action = actions[animation]?.reset().fadeIn(0.2).play()
     return () => {
-      action?.fadeOut(0.2);
-    };
-  }, [currentAnimation, mode.type, block.animation, type]);
+      action?.fadeOut(0.2)
+    }
+  }, [currentAnimation, mode.type, block.animation, type])
 
   useFrame(() => {
     if (isActive) {
-      const tag = notify() as keyof animationTagType;
-      play(tag);
+      const tag = notify() as keyof animationTagType
+      play(tag)
     }
-  });
+  })
 
   return {
     animationRef,
     currentAnimation: animationState?.[type]?.current,
-  };
+  }
 }
